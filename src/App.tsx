cc10a7e8d7ec8ea77d806628392b7b9b76a63c2e
shift1425 <shift1425@gmail.com>
2020-10-30 08:46:34 -0400

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import Routes from './routes';
-
 import 'leaflet/dist/leaflet.css';
-import './styles/global.css';
 
-export default function App() {
-  return (
-    <div className="App">
-      <Routes />
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <Routes />
+);
 
+export default App;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import CreateOrphanage from './pages/CreateOrphanage';
 import Landing from './pages/Landing';
-import Orphanage from './pages/Orphanage';
 import OrphanagesMap from './pages/OrphanagesMap';
+import CreateOrphanage from './pages/CreateOrphanage';
+import Orphanage from './pages/Orphanage';
 
-export default function Routes() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Landing} />
-        <Route path="/app" component={OrphanagesMap} />
-        <Route path="/orphanages/create" component={CreateOrphanage} />
-        <Route path="/orphanages/:id" component={Orphanage} />
-      </Switch>
-    </BrowserRouter>
-  );
-}
+const Routes: React.FC = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Landing} />
+      <Route path="/app" exact component={OrphanagesMap} />
+
+      <Route path="/orphanages/create" exact component={CreateOrphanage} />
+      <Route path="/orphanages/:id" exact component={Orphanage} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
