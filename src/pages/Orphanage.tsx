f9ad2a41c083cdf4e2ca13fda8ac9a8c0d4980f4
shift1425 <shift1425@gmail.com>
2020-10-30 09:25:50 -0400

    deploy

--- src/pages/Orphanage.tsx
+++ src/pages/Orphanage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import { FiClock, FiInfo} from "react-icons/fi";
 import { Map, Marker, TileLayer } from "react-leaflet";
-import { Link, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 import '../styles/pages/orphanage.css';
 import Sidebar from "../components/Sidebar";
