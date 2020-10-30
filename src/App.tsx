09852f7995c5359282e5c1404cbfe5402b655019
shift1425 <shift1425@gmail.com>
2020-10-30 08:40:14 -0400

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import 'leaflet/dist/leaflet.css';
-import './styles/global.css'
-
 import Routes from './routes';
 
+import 'leaflet/dist/leaflet.css';
+import './styles/global.css';
 
-function App() {
+export default function App() {
   return (
-    <Routes />
+    <div className="App">
+      <Routes />
+    </div>
   );
 }
 
-export default App;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,26 +1,20 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import Landing from './pages/Landing';
-import OrphanagesMap from './pages/OrphanagesMap';
 import CreateOrphanage from './pages/CreateOrphanage';
+import Landing from './pages/Landing';
 import Orphanage from './pages/Orphanage';
-import CreateOrphanageSucess from './pages/CreateOrphanageSucess';
-import NotCreateOrphanage from './pages/NotCreateOrphanage';
+import OrphanagesMap from './pages/OrphanagesMap';
 
-function Routes() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={Landing} />
-                <Route path="/app" component={OrphanagesMap} />
-                <Route path="/orphanages/create" component={CreateOrphanage} />
-                <Route path="/orphanages/:id" component={Orphanage} />
-                <Route path="/success" component={CreateOrphanageSucess} />
-                <Route path="/error" component={NotCreateOrphanage} />
-            </Switch>
-        </BrowserRouter>
-    );
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/app" component={OrphanagesMap} />
+        <Route path="/orphanages/create" component={CreateOrphanage} />
+        <Route path="/orphanages/:id" component={Orphanage} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
-
-export default Routes;
