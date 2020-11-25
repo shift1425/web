5fcaff5a7d15e6aef52d735aeb3e132036eef8cb
shift1425 <shift1425@gmail.com>
2020-11-25 14:45:08 -0400

    pagina de orfanado cadastrados

--- src/components/SidebarDashboard.tsx
+++ src/components/SidebarDashboard.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { FiArrowLeft } from 'react-icons/fi';
 import { useHistory } from 'react-router-dom';
 
 import mapMarkerImg from '../images/map-marker.svg';
+import power from '../images/power.svg';
+import mapPin from '../images/map-pin.svg';
+import alertCircle from '../images/alert-circle.svg';
 
 import '../styles/components/sidebar.css'
 import '../styles/components/animations.css';
+import { ManyToOne } from 'typeorm';
 
 
 export default function Sidebar() {
@@ -15,10 +18,17 @@ export default function Sidebar() {
     return (
         <aside className="app-sidebar animate-sidebar">
         <img src={mapMarkerImg} alt="Happy" />
-
+          <div>
+            <button type="button" className="rounded-button">
+              <img src={mapPin} alt="Mapa"/>
+            </button>
+            <button type="button" className="rounded-button selected">
+              <img src={alertCircle} alt="Alerta"/>
+            </button>
+          </div>
         <footer>
-          <button className="animate-sidebar" type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
+          <button className="animate-sidebar rounded-button" type="button" onClick={goBack}>
+            <img src={power} alt="sair"/>
           </button>
         </footer>
       </aside>
--- src/pages/dashboard/OrphanagesRegistered.tsx
+++ src/pages/dashboard/OrphanagesRegistered.tsx
@@ -1,24 +1,162 @@
-import React, { ChangeEvent, FormEvent, useState } from "react";
-import { Map, Marker, TileLayer } from 'react-leaflet';
-import { LeafletMouseEvent } from 'leaflet';
-
-import { FiPlus } from "react-icons/fi";
+import React from "react";
+import { Map, Marker, TileLayer } from "react-leaflet";
 
 
-import '../../styles/pages/create-orphanage.css';
-import Sidebar from "../../components/Sidebar";
+
 import mapIcon from "../../utils/mapIcon";
-import api from "../../services/api";
-import { useHistory } from "react-router-dom";
+
+import trash from "../../images/trash.svg"
+import edit from "../../images/edit.svg"
+
+
+
+import '../../styles/pages/orphanages-registered.css';
+
+import Sidebar from "../../components/SidebarDashboard";
+
 
 
 export default function OrphanagesRegistered() {
  
   return (
-    <div id="page-create-orphanage">
-    <Sidebar />
-      <main>
-       
+    <div id="orphanages-registered">
+      <Sidebar />
+      <header>
+        Orfanatos Cadastrados
+      </header>
+      <main id="content">
+        <div className="map-container">
+                <Map 
+                  center={[-15.2310837,-59.3325136]} 
+                  zoom={16} 
+                  style={{ width: '100%', height: 280 }}
+                  dragging={false}
+                  touchZoom={false}
+                  zoomControl={false}
+                  scrollWheelZoom={false}
+                  doubleClickZoom={false}
+                >
+                  <TileLayer 
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                  />
+                  <Marker interactive={false} icon={mapIcon} position={[-15.2310837,-59.3325136]} />
+                </Map>
+
+                <footer>
+                <h1>Orfanato Teste</h1>
+                  <div>
+                    <button className="rounded-button btn-edit"><img src={edit} alt="Editar"/></button>
+                    <button className="rounded-button btn-edit"><img src={trash} alt="Excluir'"/></button>
+                  </div>
+                </footer>
+              </div>
+
+
+              <div className="map-container">
+                <Map 
+                  center={[-15.2310837,-59.3325136]} 
+                  zoom={16} 
+                  style={{ width: '100%', height: 280 }}
+                  dragging={false}
+                  touchZoom={false}
+                  zoomControl={false}
+                  scrollWheelZoom={false}
+                  doubleClickZoom={false}
+                >
+                  <TileLayer 
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                  />
+                  <Marker interactive={false} icon={mapIcon} position={[-15.2310837,-59.3325136]} />
+                </Map>
+
+                <footer>
+                <h1>Orfanato Teste</h1>
+                  <div>
+                    <button className="rounded-button btn-edit"><img src={edit} alt="Editar"/></button>
+                    <button className="rounded-button btn-edit"><img src={trash} alt="Excluir'"/></button>
+                  </div>
+                </footer>
+              </div>
+
+              <div className="map-container">
+                <Map 
+                  center={[-15.2310837,-59.3325136]} 
+                  zoom={16} 
+                  style={{ width: '100%', height: 280 }}
+                  dragging={false}
+                  touchZoom={false}
+                  zoomControl={false}
+                  scrollWheelZoom={false}
+                  doubleClickZoom={false}
+                >
+                  <TileLayer 
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                  />
+                  <Marker interactive={false} icon={mapIcon} position={[-15.2310837,-59.3325136]} />
+                </Map>
+
+                <footer>
+                <h1>Orfanato Teste</h1>
+                  <div>
+                    <button className="rounded-button btn-edit"><img src={edit} alt="Editar"/></button>
+                    <button className="rounded-button btn-edit"><img src={trash} alt="Excluir'"/></button>
+                  </div>
+                </footer>
+              </div>
+
+              <div className="map-container">
+                <Map 
+                  center={[-15.2310837,-59.3325136]} 
+                  zoom={16} 
+                  style={{ width: '100%', height: 280 }}
+                  dragging={false}
+                  touchZoom={false}
+                  zoomControl={false}
+                  scrollWheelZoom={false}
+                  doubleClickZoom={false}
+                >
+                  <TileLayer 
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                  />
+                  <Marker interactive={false} icon={mapIcon} position={[-15.2310837,-59.3325136]} />
+                </Map>
+
+                <footer>
+                <h1>Orfanato Teste</h1>
+                  <div>
+                    <button className="rounded-button btn-edit"><img src={edit} alt="Editar"/></button>
+                    <button className="rounded-button btn-edit"><img src={trash} alt="Excluir'"/></button>
+                  </div>
+                </footer>
+              </div>
+
+              <div className="map-container">
+                <Map 
+                  center={[-15.2310837,-59.3325136]} 
+                  zoom={16} 
+                  style={{ width: '100%', height: 280 }}
+                  dragging={false}
+                  touchZoom={false}
+                  zoomControl={false}
+                  scrollWheelZoom={false}
+                  doubleClickZoom={false}
+                >
+                  <TileLayer 
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                  />
+                  <Marker interactive={false} icon={mapIcon} position={[-15.2310837,-59.3325136]} />
+                </Map>
+
+                <footer>
+                <h1>Orfanato Teste</h1>
+                  <div>
+                    <button className="rounded-button btn-edit"><img src={edit} alt="Editar"/></button>
+                    <button className="rounded-button btn-edit"><img src={trash} alt="Excluir'"/></button>
+                  </div>
+                </footer>
+              </div>
+
+
       </main>
     </div>
   );
